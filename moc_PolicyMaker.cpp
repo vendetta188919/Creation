c5cfc12953769d70ce704b24ea25bd757c48d535
Elena <104027694+vendetta188919@users.noreply.github.com>
2024-05-29 18:07:52 +0300

    Add files via upload

---
+++ moc_PolicyMaker.cpp
@@ -0,0 +1,153 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'PolicyMaker.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.5)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../../../../src/PolicyMaker.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'PolicyMaker.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.5. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_PolicyMaker_t {
+    QByteArrayData data[8];
+    char stringdata0[83];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_PolicyMaker_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_PolicyMaker_t qt_meta_stringdata_PolicyMaker = {
+    {
+QT_MOC_LITERAL(0, 0, 11), // "PolicyMaker"
+QT_MOC_LITERAL(1, 12, 8), // "sendData"
+QT_MOC_LITERAL(2, 21, 0), // ""
+QT_MOC_LITERAL(3, 22, 24), // "std::vector<std::string>"
+QT_MOC_LITERAL(4, 47, 7), // "onClose"
+QT_MOC_LITERAL(5, 55, 8), // "onCreate"
+QT_MOC_LITERAL(6, 64, 11), // "onShowBoxes"
+QT_MOC_LITERAL(7, 76, 6) // "iValue"
+
+    },
+    "PolicyMaker\0sendData\0\0std::vector<std::string>\0"
+    "onClose\0onCreate\0onShowBoxes\0iValue"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_PolicyMaker[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       4,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,   34,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       4,    0,   37,    2, 0x08 /* Private */,
+       5,    0,   38,    2, 0x08 /* Private */,
+       6,    1,   39,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, 0x80000000 | 3,    2,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    7,
+
+       0        // eod
+};
+
+void PolicyMaker::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<PolicyMaker *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->sendData((*reinterpret_cast< std::vector<std::string>(*)>(_a[1]))); break;
+        case 1: _t->onClose(); break;
+        case 2: _t->onCreate(); break;
+        case 3: _t->onShowBoxes((*reinterpret_cast< int(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (PolicyMaker::*)(std::vector<std::string> );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&PolicyMaker::sendData)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject PolicyMaker::staticMetaObject = { {
+    QMetaObject::SuperData::link<QDialog::staticMetaObject>(),
+    qt_meta_stringdata_PolicyMaker.data,
+    qt_meta_data_PolicyMaker,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *PolicyMaker::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *PolicyMaker::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_PolicyMaker.stringdata0))
+        return static_cast<void*>(this);
+    return QDialog::qt_metacast(_clname);
+}
+
+int PolicyMaker::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QDialog::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 4)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 4;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 4)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 4;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void PolicyMaker::sendData(std::vector<std::string> _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
